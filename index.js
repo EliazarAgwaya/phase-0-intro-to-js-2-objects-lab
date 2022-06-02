41273531025bf5fd2a221814e7050a163fd855b2
Eliazar Agwaya <eliazar.magero@student.moringaschool.com>
2022-06-02 12:49:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee={
+    name: "Eliazar",
+    streetAddress: "Nairobi",
+};
+function updateEmployeeWithKeyAndValue(employees, key, value){
+    const anotherEmployee = {...employee};   
+    anotherEmployee[key]= value;
+    return anotherEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const anotherEmployee={...employee};
+    delete anotherEmployee[key];
+    return anotherEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
